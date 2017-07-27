58cfb432eee67ccf1d386a2541fbf240f521e8ec
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-07-27 18:58:09 +0300

    vals renaming in benchmark

--- src/test/scala/scorex/crypto/authds/avltree/batch/benchmark/BatchingBenchmark.scala
+++ src/test/scala/scorex/crypto/authds/avltree/batch/benchmark/BatchingBenchmark.scala
@@ -9,10 +9,10 @@ import scorex.utils.Random
 
 object BatchingBenchmark extends App {
   val Dirname = "/tmp/iohk/avliodbbench"
-  val KL = 26
-  val VL = 8
-  val LL = 32
-  val InitilaMods = 0
+  val KeyLength = 26
+  val ValueLength = 8
+  val LabelLength = 32
+  val InitialMods = 0
   val NumMods = 2000000
 
 
@@ -20,7 +20,7 @@ object BatchingBenchmark extends App {
   new File(Dirname).mkdirs()
   new File(Dirname).listFiles().foreach(f => f.delete())
   val store = new LSMStore(new File(Dirname), keepVersions = 100)
-  val storage = new VersionedIODBAVLStorage(store, KL, VL, LL)
+  val storage = new VersionedIODBAVLStorage(store, KeyLength, ValueLength, LabelLength)
   require(storage.isEmpty)
 
 
@@ -36,8 +36,8 @@ object BatchingBenchmark extends App {
   bench()
 
   def bench(): Unit = {
-    val prover = new BatchAVLProver(KL, Some(VL), None)
-    val persProver = new PersistentBatchAVLProver(new BatchAVLProver(KL, Some(VL), None), storage)
+    val prover = new BatchAVLProver(KeyLength, Some(ValueLength), None)
+    val persProver = new PersistentBatchAVLProver(new BatchAVLProver(KeyLength, Some(ValueLength), None), storage)
 
     val Step = 2000
     digest = persProver.digest
@@ -87,9 +87,9 @@ object BatchingBenchmark extends App {
     val mods = new Array[Modification](NumMods)
 
     for (i <- 0 until NumMods) {
-      if (i == 0 || i < InitilaMods || i % 2 == 0) {
+      if (i == 0 || i < InitialMods || i % 2 == 0) {
         // with prob ~.5 insert a new one, with prob ~.5 update an existing one
-        mods(i) = Insert(Random.randomBytes(KL), Random.randomBytes(8))
+        mods(i) = Insert(Random.randomBytes(KeyLength), Random.randomBytes(8))
         numInserts += 1
       } else {
         val j = Random.randomBytes(3)
